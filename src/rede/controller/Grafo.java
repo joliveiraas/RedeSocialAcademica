04dac0c59c269e62c11a2bfc03a7c8fc5507f5d1
Nataly Barbosa Pereira <nataly.nath@aluno.ufrb.edu.br>
2025-07-03 02:04:38 -0300

    add classes

--- src/rede/controller/Grafo.java
+++ src/rede/controller/Grafo.java
@@ -1,4 +1,5 @@
 package rede.controller;
+import java.util.List;
 
 public class Grafo {
     // atributos: listaAlunos, matrizAdj
@@ -6,4 +7,30 @@ public class Grafo {
     // 1.addAluno, 2.addDisciplina, 3. buscarAluno (Nataly)
     // 4.criarRede, 5. maisConectado, 6.sugerirAmigos, 7.grauAluno (Joana)
     // 8.listarAmigos, 9. verComunidade , 10.buscarCaminho, 11.parAlunos (JP)
+
+    private List<Aluno> listaAlunos;
+    private int[][] matrizAdj;
+
+    public Grafo(List<Aluno> listaAlunos, int[][] matrizAdj) {
+        this.listaAlunos = listaAlunos;
+        this.matrizAdj = matrizAdj;
+    }
+
+    public List<Aluno> getListaAlunos() {
+        return listaAlunos;
+    }
+
+    public void setListaAlunos(List<Aluno> listaAlunos) {
+        this.listaAlunos = listaAlunos;
+    }
+
+    public int[][] getMatrizAdj() {
+        return matrizAdj;
+    }
+
+    public void setMatrizAdj(int[][] matrizAdj) {
+        this.matrizAdj = matrizAdj;
+    }
+
+
 }
--- src/rede/model/Aresta.java
+++ src/rede/model/Aresta.java
@@ -2,4 +2,38 @@ package rede.model;
 
 public class Aresta {
     // atributos: aluno1, aluno2, peso
+
+    private Aluno aluno1;
+    private Aluno aluno2;
+    private int peso;
+
+    public Aresta(Aluno aluno1, Aluno aluno2, int peso) {
+        this.aluno1 = aluno1;
+        this.aluno2 = aluno2;
+        this.peso = peso;
+    }
+
+    public Aluno getAluno1() {
+        return aluno1;
+    }
+
+    public void setAluno1(Aluno aluno1) {
+        this.aluno1 = aluno1;
+    }
+
+    public Aluno getAluno2() {
+        return aluno2;
+    }
+
+    public void setAluno2(Aluno aluno2) {
+        this.aluno2 = aluno2;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
 }
