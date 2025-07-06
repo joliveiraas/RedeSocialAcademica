751347f4f11f869163bfff5e821017869773d6ba
João Pedro <joaopedrosanti8000@gmail.com>
2025-07-06 04:13:42 -0300

    Adição da classe AddArestas, sugerirAmigos, mostrarParAlunos e auxiliares

--- src/rede/controller/Grafo.java
+++ src/rede/controller/Grafo.java
@@ -4,6 +4,8 @@ import rede.model.Aresta;
 import rede.model.Disciplina;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class Grafo {
@@ -25,18 +27,18 @@ public class Grafo {
         redimensionarMatriz();
     }
 
-    public void addDisciplina(String matricula, String nomeDisciplina, String codigo){
+    public void addDisciplina(String matricula, String nomeDisciplina, String codigo) {
         Disciplina novaDisciplina = new Disciplina(nomeDisciplina, codigo);
-        for(Aluno a : getListaAlunos()){
-            if(a.getMatricula().equals(matricula)){
+        for (Aluno a : getListaAlunos()) {
+            if (a.getMatricula().equals(matricula)) {
                 a.getListaDisciplinas().add(novaDisciplina);
             }
         }
     }
 
-    public Aluno buscarAlunoMatricula(String matricula){
-        for(Aluno a : getListaAlunos()){
-            if(a.getMatricula().equals(matricula)){
+    public Aluno buscarAlunoMatricula(String matricula) {
+        for (Aluno a : getListaAlunos()) {
+            if (a.getMatricula().equals(matricula)) {
                 return a;
             }
         }
@@ -44,9 +46,9 @@ public class Grafo {
         return null;
     }
 
-    public Aluno buscarAlunoNome(String nome){
-        for(Aluno a : getListaAlunos()){
-            if(a.getNome().equalsIgnoreCase(nome)){
+    public Aluno buscarAlunoNome(String nome) {
+        for (Aluno a : getListaAlunos()) {
+            if (a.getNome().equalsIgnoreCase(nome)) {
                 return a;
             }
         }
@@ -54,9 +56,9 @@ public class Grafo {
         return null;
     }
 
-    private void redimensionarMatriz(){
+    private void redimensionarMatriz() {
         int tamanho = getListaAlunos().size();
-        int [][] novaMatriz = new int[tamanho][tamanho];
+        int[][] novaMatriz = new int[tamanho][tamanho];
 
         for (int i = 0; i < tamanho - 1; i++) {
             for (int j = 0; j < tamanho - 1; j++) {
@@ -66,45 +68,44 @@ public class Grafo {
         setMatrizAdj(novaMatriz);
     }
 
-    public void criarRede(){ //Deve ser criada a partir de disciplinas iguais logo vai ser automatico
+    public void criarRede() { //Deve ser criada a partir de disciplinas iguais logo vai ser automatico
 
-        for(int i = 0; i < getListaAlunos().size(); i++){
-            for(int j = i + 1; j < getListaAlunos().size(); j++) {
+        for (int i = 0; i < getListaAlunos().size(); i++) {
+            for (int j = i + 1; j < getListaAlunos().size(); j++) {
                 Aluno a1 = getListaAlunos().get(i);
                 Aluno a2 = getListaAlunos().get(j);
                 int peso = contarDisciplinas(a1, a2);
                 getMatrizAdj()[i][j] = peso;
                 getMatrizAdj()[j][i] = peso; //N direcionado
-
-                // Arestas
-                if(peso > 0){
-                    Aresta aresta = a1.buscaAresta(a2);
-
-                    // Cria aresta de amizade, se ainda não existe
-                    if (aresta == null) {
-                        Aresta amizade = new Aresta(a1, a2, peso);
-                        a1.addAresta(amizade);
-                        a2.addAresta(amizade);
-                    }
-
-                    // Se já existe, atualiza peso da amizade
-                    else {
-                        aresta.setPeso(peso);
-
-                    }
-                }
-
             }
+        }
+        criarArestas();
+    }
 
+    public void criarArestas(){
+        int n = getListaAlunos().size();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int peso = getMatrizAdj()[i][j];
+                if(peso > 0 ){
+                    Aluno origem = getListaAlunos().get(i);
+                    Aluno destino = getListaAlunos().get(j);
+                    Aresta a1 = new Aresta(origem, destino, peso);
+                    Aresta a2 = new Aresta(destino, origem, peso);
+                    origem.addAresta(a1);
+                    destino.addAresta(a2);
+                }
+            }
         }
     }
 
-    private int contarDisciplinas(Aluno a1, Aluno a2){
+    private int contarDisciplinas(Aluno a1, Aluno a2) {
         int contador = 0;
 
-        for(Disciplina d1 : a1.getListaDisciplinas()){
-            for(Disciplina d2 : a2.getListaDisciplinas()){
-                if(d1.getCodigo().equals(d2.getCodigo())){ //Comparando os códigos...
+        for (Disciplina d1 : a1.getListaDisciplinas()) {
+            for (Disciplina d2 : a2.getListaDisciplinas()) {
+                if (d1.getCodigo().equals(d2.getCodigo())) { //Comparando os códigos...
                     contador++;
                 }
             }
@@ -112,16 +113,16 @@ public class Grafo {
         return contador;
     }
 
-    public void imprimirMatriz(){
+    public void imprimirMatriz() {
         System.out.println("Matriz de adjacência");
         System.out.print("     ");
-        for (Aluno a: getListaAlunos()){
+        for (Aluno a : getListaAlunos()) {
             System.out.print(a.getNome() + " - ");
         }
         System.out.println();
         for (int i = 0; i < getListaAlunos().size(); i++) {
             System.out.print(getListaAlunos().get(i).getNome() + " | ");
-            for(int j = 0; j < getListaAlunos().size(); j++){
+            for (int j = 0; j < getListaAlunos().size(); j++) {
                 System.out.print(getMatrizAdj()[i][j] + "    ");
             }
             System.out.println();
@@ -219,7 +220,6 @@ public class Grafo {
             System.out.println("Caminho não encontrado.");
             return;
         }
-
         ArrayList<String> caminho = new ArrayList<>();
         for (int i = destinoIndex; i != -1; i = anterior[i]) {
             caminho.add(0, listaAlunos.get(i).getNome());
@@ -228,6 +228,95 @@ public class Grafo {
         System.out.println("Caminho de " + origemNome + " até " + destinoNome + ": " + String.join(" -> ", caminho));
     }
 
+    public ArrayList<Aluno> sugerirAmigo(String nome, int limite){
+        ArrayList<Aluno> sugestoes = new ArrayList<>();
+        Set<Aluno> jaSugeridos = new HashSet<>();
+
+        Aluno aluno = buscarAlunoNome(nome);
+        if(aluno != null){ //Existe esse aluno
+            ArrayList<Aresta> melhoresAmigos = alunoMaiorPeso(aluno); //Lista de Melhores amigos, pode ser que tenha dois com o mesmo peso
+
+            for(Aresta aresta1: melhoresAmigos){
+                Aluno melhorAmigo = aresta1.getAluno2();
+                ArrayList<Aresta> amigoDoAmigo = melhorAmigo.getListaAresta();
+                for(Aresta aresta2: amigoDoAmigo){
+                    Aluno candidato = aresta2.getAluno2();
+                    if(!candidato.equals(aluno) && !estaConectado(aluno, candidato) && !jaSugeridos.contains(candidato)){
+                        sugestoes.add(candidato);
+                        jaSugeridos.add(candidato);
+
+                        if(sugestoes.size() == limite){
+                            return sugestoes;
+                        }
+                    }
+                }
+            }
+        }
+        return sugestoes;
+    }
+
+    public ArrayList<Aresta> alunoMaiorPeso(Aluno aluno){
+        ArrayList<Aresta> arestas = aluno.getListaAresta();
+        ArrayList<Aresta> maiores = new ArrayList<>();
+
+        if(arestas.isEmpty()){
+            return null;
+        }
+
+        int maiorPeso = -1;
+        for(Aresta a: arestas){
+
+            if(a.getPeso() > maiorPeso){
+                maiorPeso = a.getPeso();
+                maiores.clear();
+                maiores.add(a);
+            } else if(a.getPeso() == maiorPeso){
+                maiores.add(a);
+            }
+        }
+        return maiores;
+    }
+
+    private boolean estaConectado(Aluno aluno1, Aluno aluno2){
+        for(Aresta a: aluno1.getListaAresta()){
+            if(a.getAluno2().equals(aluno2)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void mostrarParAlunos(){
+
+        ArrayList<String> pares = new ArrayList<>();
+        int maiorPeso = -1;
+
+        for (int i = 0; i < getListaAlunos().size(); i++) {
+            for (int j = i + 1; j < getListaAlunos().size(); j++) {
+                int peso = getMatrizAdj()[i][j];
+
+                if(peso > maiorPeso){
+                    maiorPeso = peso;
+                    pares.clear();
+                    pares.add(getListaAlunos().get(i).getNome() + " <-> " + getListaAlunos().get(j).getNome());
+                } else if(peso == maiorPeso){
+                    pares.add(getListaAlunos().get(i).getNome() + " <-> " + getListaAlunos().get(j).getNome());
+                }
+            }
+        }
+
+        if(pares.isEmpty()){
+            System.out.println("Nenhum par encontrado!");
+        } else {
+            System.out.println("Maior número de disciplinas com pares: " + maiorPeso);
+            System.out.println("Pares com essa quantidade de disciplinas: ");
+            for(String s : pares){
+                System.out.println("-> " + s);
+            }
+        }
+
+
+    }
 
     public ArrayList<Aluno> getListaAlunos() {
         return listaAlunos;
