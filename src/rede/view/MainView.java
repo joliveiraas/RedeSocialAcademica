738e4e0a380d12ca8def7cd06ed5a557aec84b26
Nataly Barbosa Pereira <nataly.nath@aluno.ufrb.edu.br>
2025-07-09 01:23:50 -0300

    add GUI

---
+++ src/rede/view/MainView.java
@@ -0,0 +1,22 @@
+package rede.view;
+
+import javax.swing.*;
+
+public class MainView {
+    private JPanel btnCadastrarAluno;
+    private JTabbedPane tabbedPane1;
+    private JPanel JLabel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea visualizarGrafoTextArea;
+    private JButton verButton;
+    private JTextArea alunoMaisConectadoTextArea;
+    private JButton verButton1;
+    private JTextArea visualizarCaminhoEntreAlunosTextArea;
+    private JButton verButton2;
+    private JTextArea textArea1;
+    private JTextArea alunoComMaiorPesoTextArea;
+    private JButton verButton3;
+    private JTextArea visualizarParDeAlunosTextArea;
+    private JButton verButton4;
+}
